1a974692e1df6ec35d0d3c138b215d4d96d5171b
Gerald Glynn <gerald.glynn@smartbear.com>
2019-01-09 12:30:49 +0000

    Basic SPA setup 2

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>CoursesPage</h1>
+        <p>This is about...</p>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
